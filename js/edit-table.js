a647f78e4e5ff8f5ca43e058cf60aebe0aeea47b
Ezequiel <ezequiel.domenech@gmail.com>
2019-05-22 18:57:45 -0300

    ultimos avances en conjunto

--- js/edit-table.js
+++ js/edit-table.js
@@ -1,46 +1,65 @@
 "use strict"
-let btnAgregarFila = document.querySelector(".btn-agregar");
-//btnAgregarFila.addEventListener("click", agregarFila);
 let btnVaciar = document.querySelector(".btn-vaciar");
-//btnVaciar.addEventListener("click", vaciarTabla);
+btnVaciar.addEventListener("click", vaciarTabla);
 let btnInsertar = document.querySelector(".btnEnviar");
 btnInsertar.addEventListener("click", insertarFila);
-let btnagregar = document.querySelector(".btn-agregar");
-btnagregar.addEventListener("click", insertaritem);
+let btnAgregar = document.querySelector(".btn-agregar");
+btnAgregar.addEventListener("click", insertarForm);
+
+let destino = document.querySelector(".destinoSelect");
+let estadia = document.querySelector(".estadiaSelect");
+let servicio = document.querySelector(".servicioSelect");
+let pago = document.querySelector(".pagoSelect");
+// tengo que agregar un evento que escuche cambios de la tabla y llame a mostrarTabla
 
 let tabla = {
-  "destinos": ["madrid", "roma", "brasil", "mexico", "cataratas"],
-  "estadia": ["15 a 20 dias", "20 a 25 noches", "30 a 35 dias", "1 Meses", "2 Meses"],
-  "servicios": ["AllInclusive", "Vuelos y traslados", "Vuelos y Hotel", "Vuelos, hotel y traslados"],
-  "forma de pago": ["Efectivo", "Tarjeta de Debito", "Tarjeta de Credito", "Pago Express"]
+  "destinos": [],
+  "estadia": [],
+  "servicios": [],
+  "forma de pago": []
 };
-  
+let forms[
+  "form1" {
+    "destinos",
+    "estadia",
+    "servicio",
+    "pago"
+  }, 
+  "form2" {
+    "destinos",
+    "estadia",
+    "servicio",
+    "pago"
+  },
+  "form3" {
+    "destinos",
+    "estadia",
+    "servicio",
+    "pago"
+  }
+];
+mostrarTabla();
 
-function insertarFila() {
-  // elementos a agregar
-  let destino = document.querySelector(".destinoSelect");
-  let estadia = document.querySelector(".estadiaSelect");
-  let servicio = document.querySelector(".servicioSelect");
-  let pago = document.querySelector(".pagoSelect");
-  // inserción de elementos a la tabla
+function mostrarTabla {
   let tblBody = document.querySelector(".tebodi");
-  let tRow = tblBody.insertRow();
-  let tCell0 = tRow.insertCell(0);
-  tCell0.innerHTML = destino.value;
+  let tRow = tblBody.insertRow(); // aca insertamos una fila a tbody
+  let tCell0 = tRow.insertCell(0); // con esto insertamos una celda vacia en la fila
   let tCell1 = tRow.insertCell(1);
-  tCell1.innerHTML = estadia.value;
   let tCell2 = tRow.insertCell(2);
-  tCell2.innerHTML = servicio.value;
   let tCell3 = tRow.insertCell(3);
-  tCell3.innerHTML = pago.value;
 }
 
-//Esto agregaría las filas de input que genera agregarFila
-/*function agregarFila{ // Esto agrega hasta 3 grupo de input para la entrada de datos
-    // no debería haber mas de 3 form abiertos
-}*/
+function insertarFila() {
+  // inserción de elementos a la tabla
+  tabla.destinos[0] = destino.value; // aca cargamos el valor del select en el json
+  tabla.estadia[0] = estadia.value;
+  tabla.servicio[0] = servicio.value;
+  tabla.pago[0] = pago.value;
+  
+}
 
 // function vaciarTabla{} Esto vaciaría de elementos la tabla
-function insertaritem() {
+
+function insertarForm() {
   let select = document.createElement(<select>)
 }
