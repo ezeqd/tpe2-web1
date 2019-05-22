98b3172fec7e8e4dba265264eac33c3340f90b3e
Ezequiel <ezequiel.domenech@gmail.com>
2019-05-22 15:46:19 -0300

    avances en metodo de insercion de filas

--- js/edit-table.js
+++ js/edit-table.js
@@ -1,30 +1,33 @@
 "use strict"
 let btnAgregarFila = document.querySelector(".btn-agregar");
-btnAgregarFila.addEventListener("click", agregarFila);
+//btnAgregarFila.addEventListener("click", agregarFila);
 let btnVaciar = document.querySelector(".btn-vaciar");
-btnVaciar.addEventListener("click", vaciarTabla);
-let btnInsertar = document.querySelector(".btnEnviar");
+//btnVaciar.addEventListener("click", vaciarTabla);
+let btnInsertar = document.querySelector(".botonazo");
 btnInsertar.addEventListener("click", insertarFila);
 
-function vaciarTabla{} // Esto vaciaría de elementos la tabla
-
-function agregarFila{ // Esto agrega hasta 3 grupo de input para la entrada de datos
-  /*let form = '<div class="form-group">'+
-    +'<input class="form-control"placeholder="Servicios">'
-    +'<input class="form-control"placeholder="Servicios">'
-    +'<p>Destino</p>'
-    +'<select class="form-control">'
-        +'<option value="madrid">Madrid</option>'
-        +'<option value="cataratas">Cataratas</option>'
-        +'<option value="mexico">Mexico</option>'
-        +'<option value="brasil">Brasil</option>'
-        +'<option value="londres">Londres</option>'
-        +'<option value="paris">Paris</option>'
-        +'<option value="roma">Roma</option>'
-        +'<option value="berlin">Berlin</option>'
-    +'</select>'; // input a agregar */
-    // no debería haber mas de 3 form abiertos
+function insertarFila() {
+  // elementos a agregar
+  let destino = document.querySelector(".destinoSelect");
+  let estadia = document.querySelector(".estadiaSelect");
+  let servicio = document.querySelector(".servicioSelect");
+  let pago = document.querySelector(".pagoSelect");
+  // inserción de elementos a la tabla
+  let tblBody = document.querySelector(".tebodi");
+  let tRow = tblBody.insertRow();
+  let tCell0 = tRow.insertCell(0);
+  tCell0.innerHTML = destino.value;
+  let tCell1 = tRow.insertCell(1);
+  tCell1.innerHTML = estadia.value;
+  let tCell2 = tRow.insertCell(2);
+  tCell2.innerHTML = servicio.value;
+  let tCell3 = tRow.insertCell(3);
+  tCell3.innerHTML = pago.value;
 }
 
-function insertarFila {
-} //Esto agregaría las filas de input que genera agregarFila
+//Esto agregaría las filas de input que genera agregarFila
+/*function agregarFila{ // Esto agrega hasta 3 grupo de input para la entrada de datos
+    // no debería haber mas de 3 form abiertos
+}*/
+
+// function vaciarTabla{} Esto vaciaría de elementos la tabla
