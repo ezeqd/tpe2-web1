fea0204735b8c17c1874fc9b9fe15c3667a65551
Ezequiel <ezequiel.domenech@gmail.com>
2019-05-23 13:20:29 -0300

    terminado el modelado de datos. falta implementacion de metodos de muestra de datos

--- js/edit-table.js
+++ js/edit-table.js
@@ -1,37 +1,27 @@
 "use strict"
 let btnVaciar = document.querySelector(".btn-vaciar");
-     btnVaciar.addEventListener("click", vaciartabla);
-let btnInsertar = document.querySelector(".btnfinalizar");
+btnVaciar.addEventListener("click", vaciarTabla);
+
+let btnInsertar = document.querySelector(".btnEnviar");
 btnInsertar.addEventListener("click", mostrarTabla);
+
 let btnAgregar = document.querySelector(".btn-agregar");
-// btnAgregar.addEventListener("click", insertarForm);
+btnAgregar.addEventListener("click", agregarForm);
 
 let destino = document.querySelector(".destinoSelect");
 let estadia = document.querySelector(".estadiaSelect");
 let servicio = document.querySelector(".servicioSelect");
 let pago = document.querySelector(".pagoSelect");
-// tengo que agregar un evento que escuche cambios de la tabla y llame a mostrarTabla
 
-document.addEventListener('DOMContentLoaded', iniciar);
-function iniciar (event) {
+let contadorForm = 0;
 
-  destino.addEventListener("change" , function(){ //aca guardo el valor ingresado en el campo destino
-    tabla.destinos[0] = destino.value; 
-    
-  });
-  estadia.addEventListener("change" , function (){ //aca guardo el valor ingresado en el campo estadia
-    tabla.estadia[0] = estadia.value;
-  });
-  servicio.addEventListener("change" , function(){ // aca guardo el valor ingresado en el campo servicio
-    tabla.servicios[0] = servicio.value;
-  });
-  pago.addEventListener("change" , function(){// aca guardo el valor ingresado en el campo pago
-    tabla.pago[0] = pago.value;
-  });
-}
-
-
-
+let forms[
+  {
+    "destinos":"",
+    "estadia":"",
+    "servicio":"",
+    "pago":""
+  }]
 
 let tabla = {
   "destinos": [],
@@ -39,56 +29,43 @@ let tabla = {
   "servicios": [],
   "pago": []
 };
+
+function agregarForm (){
+  if (contadorForm<2){
+    let nuevoFormulario = {"destinos":"", "estadia":"", "servicios":"", "pago":""}
+    forms.push(nuevoFormulario); // .push() inserta el json nuevoFormulario en el arreglo forms
+    contadorForm++;
+  }
+  mostrarTabla();
+}
+
 function mostrarTabla () {
   let tblBody = document.querySelector(".tebodi")
   let tRow = tblBody.insertRow(); // aca insertamos una fila a tbody
-  let tCell0 = tRow.insertCell(0); // con esto insertamos una celda vacia en la fila
-  let tCell1 = tRow.insertCell(1);
-  let tCell2 = tRow.insertCell(2);
-  let tCell3 = tRow.insertCell(3);
-  tCell0.innerHTML = tabla.destinos[0]; //aca tomamos el valor de la celda y le pintamos el valor ingresado en el form
+  for (let celdas = 0; celdas < 4; celdas++){
+    // con esto insertamos una celda vacia en la fila. sabemos que tenemos 4 celdas por fila (row)
+    let tCell[celdas] = tRow.insertCell(celdas);
+  }
+
+  /*tCell0.innerHTML = tabla.destinos[0]; //aca tomamos el valor de la celda y le pintamos el valor ingresado en el form
   tCell1.innerHTML = tabla.estadia[0]; // lo mismo para cada una de las celdas
   tCell2.innerHTML = tabla.servicios[0];
-  tCell3.innerHTML = tabla.pago[0]; 
+  tCell3.innerHTML = tabla.pago[0];*/
 }
-function vaciartabla (tabla) {
-  
- }
-iniciar();
 
+function vaciarTabla (tabla) {
+  let cantTabla = Object.keys(tabla).length; // Object.keys devuelve un arreglo con las prop del objeto. de eso, hacemos length
+  for (let i = 0; i < cantTabla; i++){
+    tabla.pop(); // elimina el ultimo elemento del json
+  }
+}
 
+function mostrarForm(forms){
+  let formsHTML = document.querySelector(."form-container");
+  for (let i = 0; i < contadorForm; i++){
+    //aca mostrar forms[i]
+  }
 
+}
 
-
-
-
-// let forms[
-  //     "form1" {
-    //             "destinos"
-    //             "estadia"
-    //             "servicio"
-    //             "pago"
-    //     }, 
-    //   "form2" {
-      //     "destinos",
-      //     "estadia",
-      //     "servicio",
-      //     "pago"
-      //   },
-      //   "form3" {
-        //     "destinos",
-        //     "estadia",
-        //     "servicio",
-        //     "pago"
-        //   }
-        // ];
-        // mostrarTabla();
-        
-
-
-
-
-
-// function insertarForm() {
-//   let select = document.createElement(<select>)
-// }
+/*function insertarForm() {} */
